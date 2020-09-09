860dc1881f05d6e4e1c6aee04854e7ce17e70ac4
Rob Sangar <robsangar@gmail.com>
2020-09-09 21:06:24 +0100

    IoC setup and basic input reading

---
+++ PdfCreator/PdfCreator.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfCreator
+{
+    public class PdfCreator
+    {
+        private readonly IConfiguration configuration;
+        private const string inputFileSettingName = "inputFilePath";
+
+        public PdfCreator(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Run()
+        {
+            var inputFilePath = this.configuration.GetValue<string>(inputFileSettingName);
+            var inputArray = System.IO.File.ReadAllLines(inputFilePath);
+
+        }
+        
+
+
+    }
+}
--- PdfCreator/Program.cs
+++ PdfCreator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace PdfCreator
 {
@@ -6,7 +8,36 @@ namespace PdfCreator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Configuration setup
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            //IoC registration
+            var services = new ServiceCollection();
+            services.AddSingleton(configuration);
+            services.AddTransient<PdfCreator>();
+
+            var serviceProvider = services.BuildServiceProvider();
+            var scope = serviceProvider.CreateScope();
+
+            //Run the program
+            try
+            {
+                scope.ServiceProvider.GetRequiredService<PdfCreator>().Run();
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+                Console.ReadLine();
+            }
+
+            //IoC disposal
+            if (serviceProvider != null && serviceProvider is IDisposable)
+            {
+                serviceProvider.Dispose();
+            }
+
         }
     }
 }
