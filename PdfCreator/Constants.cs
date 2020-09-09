13bde55d07f19b8d3a5b0ce5fa9e06d8b5647303
Rob Sangar <robsangar@gmail.com>
2020-09-09 22:42:50 +0100

    Added unit tests for empty and non-existent input files.

---
+++ PdfCreator/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfCreator
+{
+    public static class Constants
+    {
+        public static string EMPTY_INPUT_ERROR = "The input file at {0} was empty. Please give it some contents and re-run the program.";
+
+        public static string INPUT_FILE_NAME_SETTING = "inputFilePath";
+    }
+}
--- PdfCreator/FileReaders/FileReader.cs
+++ PdfCreator/FileReaders/FileReader.cs
@@ -7,9 +7,6 @@ namespace PdfCreator
 {
     public class FileReader : IFileReader
     {
-        private const string INPUT_FILE_NAME_SETTING = "inputFilePath";
-        private const string EMPTY_INPUT_ERROR = "The input file at {0} was empty. Please give it some contents and re-run the program.";
-
         private readonly IConfiguration configuration;
 
         public FileReader(IConfiguration configuration)
@@ -19,7 +16,7 @@ namespace PdfCreator
 
         public void ReadInput()
         {
-            var inputFilePath = this.configuration.GetValue<string>(INPUT_FILE_NAME_SETTING);
+            var inputFilePath = this.configuration.GetValue<string>(Constants.INPUT_FILE_NAME_SETTING);
 
             //Look for the file and read its contents - this will throw an error if the file is not found.
             var inputArray = System.IO.File.ReadAllLines(inputFilePath);
@@ -27,7 +24,7 @@ namespace PdfCreator
             //Check to see if the file contained no contents.
             if (inputArray == null || inputArray.Length == 0)
             {
-                throw new Exception(string.Format(EMPTY_INPUT_ERROR, inputFilePath));
+                throw new FormatException(string.Format(Constants.EMPTY_INPUT_ERROR, inputFilePath));
             }
 
             //Map the input to a list of commands.
